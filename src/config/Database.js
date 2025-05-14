7a67e929b8b5b0b4e658496e00e5ab0e74653b20
Bhushan Bankar <bankarbhushan18@gmail.com>
2025-05-14 06:48:20 +0530

    database compus to atlast

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(
     // "mongodb+srv://user:User123@namstedev.hp3ej.mongodb.net/DevTinder"
-    "mongodb://localhost:27017/DevTinder"
+    "mongodb+srv://user:e1PBcO8lbn769HlZ@cluster0.7bfll.mongodb.net/DevTinder"
+    // "mongodb://localhost:27017/DevTinder"
   );
 };
 
